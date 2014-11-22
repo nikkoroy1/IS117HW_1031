3b8c5737b9df48c18fad5ea8194adee32cc99596
Nikko Roy <nr82@njit.edu>
2014-11-22 14:54:31 -0500

    took out opacity

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
   $( "#main_content" ).css("background-color", "red");
   $( "#branding img" ).click(function() {
      $( "#main_content" ).animate({
-       opacity: 0.25,
        left: "+=50",
        height: "toggle"
        }, 5000, function() {
