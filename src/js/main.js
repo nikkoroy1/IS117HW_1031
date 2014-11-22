43d0f38fa17719d17e876c5b943e659009abd569
Nikko Roy <nr82@njit.edu>
2014-11-22 14:22:32 -0500

    added animation to branding

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $("#main_content").css("background-color", "red");
-  $("#branding img").click(function() {
-    $("main_content").animate({
-      opacity: 0.25,
-      left: "+=50",
-      height: "toggle"
-      }, 5000, function() {
-      });//end animate
+  $( "#main_content" ).css("background-color", "red");
+  $( "#branding img" ).click(function() {
+     $( "#main_content" ).animate({
+       opacity: 0.25,
+       left: "+=50",
+       height: "toggle"
+       }, 5000, function() {
+     });// animation complete
   });//end click
-});
+});//end ready
