7f4a0fe0dcb2ed23673927bbf82e3c36331aa615
Nikko Roy <nr82@njit.edu>
2014-11-23 18:00:24 -0500

    added javascript for register form

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,50 @@
-$(document).ready(function() {
-  $( "#main_content" ).css("background-color", "red");
+ 
+  (function($,W,D)
+  {
+    var JQUERY4U = {};
+
+    JQUERY4U.UTIL =
+    {
+
+      setupFormValidation: function()
+      {
+
+        $("#register-form").validate({
+	    rules: {
+	        firstname: "required",
+		lastname: "required",
+		email: {
+		    required: true,
+		    email: true
+		},
+		password: {
+		    required: true,
+		    minlength: 8
+		},
+		agree: "required"
+	},
+	messages: {
+	    firstname: "Please enter your firstname",
+	    lastname: "Please enter your lastname",
+	    password: {
+	    	required: "Please provide a password",
+		minlength: "Your password must be at least 8 characters long"
+	    },
+	    email: "Please enter a valid email address",
+	    agree: "Please accept our policy"
+	},
+	submitHandler: function(form){
+	    form.submit();
+	}
+     });
+    }
+  }
+
+  $(D).ready(function($){
+      JQUERY4U.UTIL.setupFormValidation();
+  });
+})(jQuery, window, document);
+
   $( "#branding img" ).click(function() {
      $( "#main_content" ).animate({
        left: "+=50",
@@ -7,4 +52,4 @@ $(document).ready(function() {
        }, 5000, function() {
      });// animation complete
   });//end click
-});//end ready
+
